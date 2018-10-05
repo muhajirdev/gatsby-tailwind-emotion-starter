48ba56ed2872fc21537a6cb34b42fe30733f15f6
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-05 20:02:26 +0700

    update content on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby + Tailwind CSS + Emotion js site</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
