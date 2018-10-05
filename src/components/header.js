f1ed8e529e6e9bcb6badfa6094380f792fd73cb5
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-05 17:37:26 +0700

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'react-emotion'
+
+const Text = styled.p`
+  ${tw`bg-red`};
+`
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+	<Text>a</Text>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+export default Header
